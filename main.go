cf01477920c2ca20c94904ded309b396472c364d
shaiq <funky.shaiq123@gmail.com>
2026-03-03 22:45:14 +0530

    added basic task creation endpoint"

--- main.go
+++ main.go
@@ -4,9 +4,33 @@ import (
 	"net/http"
 )
 
+type Task struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var tasks = make(map[int]Task)
+var currentID = 1
+
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
+	})
+
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			task := Task{
+				ID:    currentID,
+				Title: "Sample Task",
+				Done:  false,
+			}
+			tasks[currentID] = task
+			currentID++
+			w.Write([]byte("Task Created"))
+			return
+		}
+		w.Write([]byte("Method Not Allowed"))
 	})
 
 	http.ListenAndServe(":8080", nil)
